e3105cc1a5907cfec13cc5c51c9d563548831b0f
shinomontaz@gmail.com <shinomontaz@gmail.com>
2019-10-06 00:34:13 +0300

    time quants returned

--- core/genetic.go
+++ core/genetic.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"planeplanner/types"
+	"sort"
 
 	"github.com/shinomontaz/ga"
 )
 
+// type Schedule struct {
+// 	jobs     []*types.Job
+// 	planes   []*types.Plane
+// 	brigades []*types.Brigade
+// 	List     map[*types.Plane]map[*types.Job]*types.Employee //
+// }
+
 type Schedule struct {
+	List  [24 * 60 / 5]map[*types.Employee]*types.Job // ключи в этом массиве есть порядковый номер 5-минутного кванта времени
+	List2 map[*types.Employee]map[int]*types.Job      // ключи в этом map[int] массиве есть порядковый номер 5-минутного кванта времени
+	jobs2 map[*types.Job]int                          // значение тут есть порядковый номер 5-минутного кванта
+
 	jobs     []*types.Job
 	planes   []*types.Plane
 	brigades []*types.Brigade
-	List     map[*types.Plane]map[*types.Job]*types.Employee //
-	//	Table map[Employee]map[time.Time]*Job // расписание с точки зреня сотрудников
 }
 
 func algorithm(planes []*types.Plane, jobs []*types.Job, brigades []*types.Brigade) *Schedule {
@@ -28,11 +38,31 @@ type ScheduleFactory struct {
 
 func (sf *ScheduleFactory) Create() ga.Individual {
 	// собираем случайную особь так, чтобы уложиться в лимиты в конфиге
-	//	indexes := rand.Perm(len(sf.jobs))
+	// отсортировать работы по порядку времени начала - время возможного начала определяется прилетом самолета
+	// по сути надо отсортировать самолеты
+	// затем берем последовательно работы данного самолета и назначаем на свободных сотрудников
+	sort.Slice(sf.planes, func(i, j int) bool {
+		//		return sf.planes[i].Arrival.Before(sf.planes[j].Arrival)
+		return sf.planes[i].Arrival.Before(sf.planes[j].Arrival)
+	})
+
 	schedule := &Schedule{
 		jobs:     sf.jobs,
 		planes:   sf.planes,
 		brigades: sf.brigades,
+		// 	List  [24 * 60 / 5]map[*types.Employee]*types.Job // ключи в этом массиве есть порядковый номер 5-минутного кванта времени
+		// List2 map[*types.Employee]map[int]*types.Job      // ключи в этом map[int] массиве есть порядковый номер 5-минутного кванта времени
+		// jobs2 map[*types.Job]int  // значение тут есть порядковый номер 5-минутного кванта
+	}
+
+	for _, plane := range sf.planes {
+		for _, job := range plane.Jobs {
+			schedule.jobs2[job] = plane.Arrival2
+		}
+	}
+
+	for j, quant := range schedule.jobs2 {
+
 	}
 
 	return schedule
--- core/types/types.go
+++ core/types/types.go
@@ -6,6 +6,7 @@ type Employee struct {
 	Id    int
 	Start time.Time
 	End   time.Time
+	Shift []int // список квантов времени, когда сотрудник доступен
 }
 
 type Brigade struct {
@@ -18,17 +19,20 @@ type Job struct {
 	Id      int
 	Parent  *Job
 	Brigade *Brigade
-	Time    int
+	Time    int // количество 5-минутных квантов времени для выполнения работы
 	Count   int // число рабочих для исполнения
 }
 
 type Plane struct {
-	Name    string
-	Jobs    []*Job
-	Arrival time.Time
+	Name     string
+	Jobs     []*Job
+	Arrival  time.Time
+	Arrival2 int // номер 5-минутного кванта времени, когда самолет прилетел
 }
 
 type Schedule struct {
-	List map[*Plane]map[*Job]Employee //
-	//	Table map[Employee]map[time.Time]*Job // расписание с точки зреня сотрудников
+	List  [24 * 60 / 5]map[*Employee]*Job // ключи в этом массиве есть порядковый номер 5-минутного кванта времени
+	List2 map[*Employee]map[int]*Job      // ключи в этом map[int] массиве есть порядковый номер 5-минутного кванта времени
 }
+
+type Day [24 * 60 / 5]struct{}
